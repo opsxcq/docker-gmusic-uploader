a76e4f76a081c9cfe32740b5255b4990204b9e86
OPSXCQ <opsxcq@strm.sh>
2016-12-30 18:32:23 -0200

    fixes

--- main.sh
+++ main.sh
@@ -17,11 +17,19 @@ echo '[+] Starting music upload job'
 
 while true
 do
-    /upload.py --cred /auth --delete-on-success --match --uploader-id $UPLOADER_MAC
-    if [ -z "$AUTH_TOKEN" ]
+    count=$(find /data -name '*.mp3' | wc -l)
+    if [ $count -eq 0 ]
     then
-        AUTH_TOKEN=$(cat /auth.cred | base64 -w 0)
-        echo '[+] Generated AUTH_TOKEN, please save it for further use'
-        echo $AUTH_TOKEN
+        echo '[+] No new musics do upload at ('$(date)'), waiting'
+        sleep 120
+    else
+        echo '[+] Found '$count' musics to upload'
+        /upload.py --cred /auth --delete-on-success --match --uploader-id $UPLOADER_MAC
+        if [ -z "$AUTH_TOKEN" ]
+        then
+            AUTH_TOKEN=$(cat /auth.cred | base64 -w 0)
+            echo '[+] Generated AUTH_TOKEN, please save it for further use'
+            echo $AUTH_TOKEN
+        fi
     fi
 done
